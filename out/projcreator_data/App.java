945e2af58099fb43880f196375ed85e4385cb6c9
hatuncev <hatuncev.ku@kvant-telecom.ru>
2019-05-08 21:49:00 +0300

    fix

---
+++ out/projcreator_data/App.java
@@ -0,0 +1,21 @@
+package PROJCREATOR_PACKAGE_NAME;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+
+    public static void main(String[] args) {
+
+        new App();
+    }
+    
+    
+
+    App() {
+
+    }
+
+}
